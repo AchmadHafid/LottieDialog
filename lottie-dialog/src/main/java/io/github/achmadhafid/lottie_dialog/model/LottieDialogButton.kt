f5f6091c1b9122e52f620d5efa32e321dbc7b0cb
hafid <ach.hafid@gmail.com>
2021-09-29 16:00:04 +0700

    support button with corner radius

--- lottie-dialog/src/main/java/io/github/achmadhafid/lottie_dialog/model/LottieDialogButton.kt
+++ lottie-dialog/src/main/java/io/github/achmadhafid/lottie_dialog/model/LottieDialogButton.kt
@@ -1,6 +1,7 @@
 package io.github.achmadhafid.lottie_dialog.model
 
 import android.app.Dialog
+import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.google.android.material.button.MaterialButton
@@ -21,6 +22,8 @@ data class LottieDialogButton(
     @MaterialButton.IconGravity
     var iconGravity: Int? = null,
     var actionDelay: Long? = null,
+    @DimenRes
+    var cornerRadiusRes: Int? = null,
     internal var onClickListener: ((Dialog) -> Unit)? = null
 ) {
     internal operator fun invoke(
@@ -28,10 +31,11 @@ data class LottieDialogButton(
         button: MaterialButton,
         coroutineScope: CoroutineScope
     ) {
-        textRes    ?.let { button withTextRes it }
-        text       ?.let { button.text = it }
-        iconRes    ?.let { button.setIconResource(it) }
+        textRes?.let { button withTextRes it }
+        text?.let { button.text = it }
+        iconRes?.let { button.setIconResource(it) }
         iconGravity?.let { button.iconGravity = it }
+        cornerRadiusRes?.let { button.setCornerRadiusResource(it) }
 
         with(button) {
             onSingleClick {
